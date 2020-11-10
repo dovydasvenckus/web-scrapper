f8f3927cdb8ee3765be5c43a1522b1d3b337f4e1
Dovydas Venckus <dovydas.venckus@live.com>
2020-11-10 21:56:02 +0200

    Allow custom url and return html as body

--- scrape-func/index.js
+++ scrape-func/index.js
@@ -2,13 +2,14 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const getData = require('./scraper.js');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    context.log(await getData('https://en.wikipedia.org/wiki/ZAZ_Zaporozhets'))
-
+    const url = req.body && req.body.name;
+    if (url) {
+        context.res = {
+            body: getData(url)
+        };
+        return;
+    }
     context.res = {
-        body: responseMessage
-    };
+        status: 400
+    }
 }

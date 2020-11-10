b94ee398f22a6bd62319bf06b7eefa3e638a9637
Dovydas Venckus <dovydas.venckus@live.com>
2020-11-10 21:58:03 +0200

    Use correct body property as url

--- scrape-func/index.js
+++ scrape-func/index.js
@@ -2,7 +2,7 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const getData = require('./scraper.js');
-    const url = req.body && req.body.name;
+    const url = req.body && req.body.url;
     if (url) {
         context.res = {
             body: getData(url)

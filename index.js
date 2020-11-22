b3ce26162d165b52d62dd2c6410bbfff370c7082
Dovydas Venckus <dovydas.venckus@live.com>
2020-11-22 19:01:58 +0200

    Do not hand on error, when running locally

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ var app = express();
 app.use(express.json())
 
 app.post("/scrape", async (req, res, next) => {
-  console.log(req.body)
-  if (req.body && req.body.url) {
-    
-    const data = await getData(req.body)
-    res.send(data)
-    res.status(200)
+  try {
+    if (req.body && req.body.url) {
+      const data = await getData(req.body)
+      res.send(data)
+      res.status(200)
+    }
+    else {
+      res.status(400)
+    }
   }
-  else {
-    res.status(400)
+  catch(ex) {
+    next(ex)
   }
 })
 
